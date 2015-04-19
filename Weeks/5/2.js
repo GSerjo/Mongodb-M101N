11a5843f2ba07574e3ff5e1c8a16a839bce27218
GSerjo <sedot8@gmail.com>
2015-04-19 23:49:23 +0300

    + 2, 4

---
+++ Weeks/5/2.js
@@ -0,0 +1,10 @@
+db.zips.findOne()
+db.zips.aggregate([
+// {$match: {state: {$in: ["CT", "NJ"]}}},
+{$match: {state: {$in: ["CA", "NY"]}}},
+{$match: {pop: {$gt: 25000}}},
+{$group: {_id: {state: "$state", city: "$city"}, pop_sum: {$sum: "$pop"}}},
+{$group: {_id: "$_id.state", pop_avg: {$avg: "$pop_sum"}}}
+])
+
+
