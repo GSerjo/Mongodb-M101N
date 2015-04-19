a2d0ea8a5df2be60363025b678a93bb92f34fd52
GSerjo <sedot8@gmail.com>
2015-04-20 01:13:30 +0300

    + 3

--- Weeks/5/4.js
+++ Weeks/5/4.js
@@ -8,7 +8,8 @@ db.zips.aggregate([
     }
 },
 {$match: {first_char: {$in: ["0","1","2","3","4","5","6","7","8","9"]}}},
-{$group: {_id: "$first_char", pop_sum: {$sum: "$pop"}}}
+{$group: {_id: "$first_char", pop_sum: {$sum: "$pop"}}},
+{$group: {_id: null, pop_sum: {$sum: "$pop_sum"}}}
 ])
 
 
